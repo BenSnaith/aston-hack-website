021692e606e307a5ad13a8e91087b2ef0c844b96
BenSnaith <bsnaith.bs@icloud.com>
2024-11-03 16:53:39 +0000

    added countdown component

---
+++ src/app/countdown/countdown.component.ts
@@ -0,0 +1,44 @@
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-countdown',
+  standalone: true,
+  imports: [],
+  templateUrl: './countdown.component.html',
+  styleUrl: './countdown.component.css'
+})
+export class CountdownComponent implements AfterViewInit {
+  date: any;
+  now: any;
+  targetDate: Date = new Date(2025, 2, 22);
+  targetTime: any = this.targetDate.getTime();
+  difference: number;
+
+  @ViewChild('days', { static: true}) days: ElementRef;
+  @ViewChild('hours', { static: true}) hours: ElementRef;
+  @ViewChild('minutes', { static: true}) minutes: ElementRef;
+  @ViewChild('seconds', { static: true}) seconds: ElementRef;
+  @ViewChild('countdown-container', {static: true}) countdownContainer: HTMLDivElement;
+
+  ngAfterViewInit() {
+    setInterval(() => {
+      this.tickTock();
+      this.difference = this.targetTime - this.now;
+      this.difference = this.difference / (1000 * 60 * 60 * 24);
+
+      // I'm not sure why this works to be honest...
+      !isNaN(this.days.nativeElement.innerText)
+        ? (this.days.nativeElement.innerText = Math.floor(this.difference))
+        : (this.days.nativeElement.innerText = Math.floor(this.difference));
+    }, 1000)
+  }
+
+  tickTock() {
+    this.date = new Date();
+    this.now = this.date.getTime();
+    this.days.nativeElement.innerText = Math.floor(this.difference);
+    this.hours.nativeElement.innerText = 23 - this.date.getHours();
+    this.minutes.nativeElement.innerText = 60 - this.date.getMinutes();
+    this.seconds.nativeElement.innerText = 60 - this.date.getSeconds();
+  }
+}
