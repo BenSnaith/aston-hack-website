3c87f2fee0c23f90325d4f95ec7d8d5c9f121a92
Ben Snaith <bsnaith.bs@icloud.com>
2024-11-26 10:03:00 +0000

    feat: sponsor section

---
+++ src/app/components/sponsor-card/sponsor-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sponsor-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './sponsor-card.component.html',
+  styleUrl: './sponsor-card.component.css'
+})
+export class SponsorCardComponent {
+
+}
