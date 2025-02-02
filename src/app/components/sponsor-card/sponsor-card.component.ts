908bdf1c38174ac4f7e996fc04871f5b1f3b16d4
BenSnaith <bsnaith.bs@icloud.com>
2025-02-02 15:32:23 +0000

    feat: added majestic sponsor card

--- src/app/components/sponsor-card/sponsor-card.component.ts
+++ src/app/components/sponsor-card/sponsor-card.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-sponsor-card',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './sponsor-card.component.html',
   styleUrl: './sponsor-card.component.css'
 })
 export class SponsorCardComponent {
-
+  @Input({ required: true }) image: string = '';
+  @Input({ required: true }) imageAlt: string = '';
+  @Input({ required: true }) websiteHref: string = '';
 }
--- src/app/components/sponsors/sponsors.component.ts
+++ src/app/components/sponsors/sponsors.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from '../header/header.component';
+import {SponsorCardComponent} from '../sponsor-card/sponsor-card.component';
 
 @Component({
   selector: 'app-sponsors',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, SponsorCardComponent],
   templateUrl: './sponsors.component.html',
   styleUrl: './sponsors.component.css'
 })
